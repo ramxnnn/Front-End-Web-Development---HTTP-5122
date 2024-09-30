b4953ed675c96059ad5df835bfc954d1ce62b8c3
ramxnnn <ramanpannu1003@gmail.com>
2024-09-30 13:21:36 -0400

    done with Lab 4

---
+++ JS-LAB-4-Functions-24f/lab-4-2.js
@@ -0,0 +1,42 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+function calAverage(num1, num2, num3, num4, num5) {
+    var total = num1 + num2 + num3 + num4 + num5;
+    var average = total / 5;
+    return average;
+}
+
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+console.log(calAverage(5, 10, 15, 20, 25)); //console output with the result 15
+
+// Creating five variables (one for each course in this semester)
+// Variables for Sucess
+// var IXD5106 = 78; 
+// var HTTP5121 = 82; 
+// var HTTP5126 = 91; 
+// var HTTP5125 = 67; 
+// var HTTP5122 = 85; 
+
+// Variables for review required
+
+var IXD5106 = 50; 
+var HTTP5121 = 50; 
+var HTTP5126 = 50; 
+var HTTP5125 = 50; 
+var HTTP5122 = 50; 
+
+
+// Using my custom function to create a logic that will send the success popup if my functions returns an overall average of 70 or more.
+
+var overallAverage = calAverage(IXD5106, HTTP5121, HTTP5126, HTTP5125, HTTP5122);
+
+// if logic for this
+
+if (overallAverage >= 70) {
+    alert("Success! Your average is " + overallAverage);
+} else {
+    alert("Review required. Your average is " + overallAverage);
+}
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
---
+++ JS-LAB-4-Functions-24f/lab-4-3.js
@@ -0,0 +1,30 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  WALKING THE DOG
+
+
+//################## CREATE YOUR checkTemp FUNCTION
+// This function will check if the temperature is safe for walking a dog.
+// It expects to receive one parameter: currentTemp (a number representing the temperature).
+// It will return true if the temperature is safe (between -10 and 30 degrees), and false if it is too hot or too cold.
+
+// Creating a function for checking the temprature for too cold or too hot 
+
+function checkTemp(currentTemp) {
+    if (currentTemp > 30 || currentTemp < -10) {
+        return false; // Too hot or too cold
+    } else {
+        return true; // Safe to walk
+    }
+}
+
+// Creating a variable for the user and passing it to the parameter for checking the function.
+
+var currentTemperature = prompt("What is the current temperature?"); // To Ask for current temperature as the input
+
+if (checkTemp(currentTemperature) === false) {
+    alert("Yikes! This is no weather for dog walking!"); // This is the Message for too hot or too cold to walk the dog
+} else {
+    alert("You’re good, have a nice walk!"); // Message for safe temperature
+}
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
